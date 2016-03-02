fb46c3465b9914146b71a8f77fa2350d455fafb5
Kevin Love <klove@flmnh.ufl.edu>
2016-03-02 14:37:27 -0500

    Fixed tests paths

--- tests/testthat/test-json.R
+++ tests/testthat/test-json.R
@@ -1,5 +1,5 @@
 ## Test structure of collection JSON files
-setwd("/home/travis/build/iDigBio/idb-us-collections/data/")
+setwd("/home/travis/build/iDigBio/idb-us-collections/data-raw/")
 context("test attributes")
 test_that("check JSON file attributes against stub", {
         stub <- jsonlite::fromJSON("../stub.json")
