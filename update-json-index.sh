55af46f38fb0e7c008de507c29624d8e00f35007
Kevin Love <kevinlove@ufl.edu>
2016-04-15 23:55:09 -0400

    Update update-json-index.sh

--- update-json-index.sh
+++ update-json-index.sh
@@ -14,6 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #go into directory and create our index file
   cd json-index/collections
   sed -s '$a,' * > ../index.json
+  sed -i '$ d' ../index.json
   sed -i '1i [' ../index.json
   sed -i '$a ]' ../index.json
 
