edefcc01c159b8bd5141168be7ceab450266383c
Kevin Love <kevinlove@ufl.edu>
2016-04-18 13:12:21 -0400

    Update update-json-index.sh

--- update-json-index.sh
+++ update-json-index.sh
@@ -7,11 +7,11 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone json-index branch
-  git clone --quiet --branch=json-index https://${GH_TOKEN}@github.com/iDigBio/idb-us-collections.git  json-index > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/iDigBio/idb-us-collections.git  gh-pages > /dev/null
   df -h
 
   #go into directory and create our index file
-  cd json-index/collections
+  cd gh-pages/collections
   sed -s '$a,' * > ../index.json
   sed -i '$ d' ../index.json
   sed -i '1i [' ../index.json
@@ -19,8 +19,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #add, commit and push files
   git add ../index.json
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to json-index"
-  git push -fq origin json-index > /dev/null
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
 
-  echo -e "Done magic with index\n"
+  echo -e "Done magic with GitHub pages\n"
 fi
